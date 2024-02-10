efb2b4fb27fdebc6507993417885c13911f02e54
Dela Cruz, Michael Angelo <134262194+michinnn@users.noreply.github.com>
2024-02-10 23:32:18 +0800

    ...

--- src/Components/header/NavBar.jsx
+++ src/Components/header/NavBar.jsx
@@ -11,10 +11,10 @@ const NavBar = () => {
            </div>
 
              <div className='flex gap-14 '>
-              <a href="/" className='text-gray-300 font-semibold hover:text-[#DC8857] hover:font-semibold'>About</a>
-              <a href="/PetAdoption/testimonialpage" className='text-gray-300 font-semibold hover:text-[#DC8857] hover:font-semibold'>Testimonies</a>
-              <a href="/PetAdoption/adoptpage" className='text-gray-300 font-semibold hover:text-[#DC8857] hover:font-semibold'>Adopt</a>
-              <a href="/PetAdoption/profilepage"><FaUser className='text-[#DC8857]/50 hover:text-[#DC8857] mt-1 cursor-pointer'/></a>
+              <a href="/#/" className='text-gray-300 font-semibold hover:text-[#DC8857] hover:font-semibold'>About</a>
+              <a href="/PetAdoption/#/testimonialpage" className='text-gray-300 font-semibold hover:text-[#DC8857] hover:font-semibold'>Testimonies</a>
+              <a href="/PetAdoption/#/adoptpage" className='text-gray-300 font-semibold hover:text-[#DC8857] hover:font-semibold'>Adopt</a>
+              <a href="/PetAdoption/#/profilepage"><FaUser className='text-[#DC8857]/50 hover:text-[#DC8857] mt-1 cursor-pointer'/></a>
              </div>
 
         </div>
